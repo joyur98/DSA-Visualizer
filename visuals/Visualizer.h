d2bb35caa648b0e9f93b42f342f5944c04f061eb
joyur98 <kcruyoj7@gmail.com>
2026-02-24 12:40:52 +0545

    fixed a compilation bug

--- visuals/Visualizer.h
+++ visuals/Visualizer.h
@@ -1,10 +1,10 @@
 #pragma once // makes it so that this header file is only included once
 
 #include "raylib.h" //raylib header file for graphic functions
-#include "structures/Stack.h" //stack data structure header
-#include "structures/Queue.h" //queue data structure header
-#include "structures/LinkedList.h" //linked List data structure header
-#include "structures/BinaryTree.h" //binary Tree data structure header
+#include "Stack.h" //stack data structure header
+#include "Queue.h" //queue data structure header
+#include "LinkedList.h" //linked List data structure header
+#include "BinaryTree.h" //binary Tree data structure header
 
 //functions to draw and visualzie the different data strucutres
 class Visualizer {
